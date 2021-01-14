ee9770585a01ce1653dbd69517f73a2479d501bf
Thomas Keith <tkthomaskeith@gmail.com>
2021-01-13 17:55:12 -0700

    Setting up First Project

---
+++ Week 1_Basic Shapes/sketch.js
@@ -0,0 +1,24 @@
+function setup() {
+  createCanvas(640,360);
+}
+
+function draw() {
+
+  // Background with Border
+  fill(0);
+  background(255,0,98);
+  rect(5,5,630,350);
+
+// Lower Right-Hand Text
+
+fill(255);
+textSize(16);
+text('Thomas Keith',532,350);
+
+// Upper Left-Hand TITLE
+
+fill(255);
+textSize(16);
+text('Basic Shapes',10,20)
+
+}
