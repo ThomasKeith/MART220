9d92ee2a81b7d276d2699f45b17a2275080e2951
Thomas Keith <tkthomaskeith@gmail.com>
2021-01-20 16:21:50 -0700

    Update sketch.js

--- Week 2/sketch.js
+++ Week 2/sketch.js
@@ -3,12 +3,8 @@ function setup() {
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
-  }
+  
+  
 
-  // background(220);
-  ellipse(mouseX,mouseY,80,80);
+ 
 }
