6949eb43812864cf5ccb911b451ca4163c4bce29
Thomas Keith <tkthomaskeith@gmail.com>
2021-02-07 19:07:43 -0700

    Update sketch.js

--- Week 4/sketch.js
+++ Week 4/sketch.js
@@ -1,64 +1,141 @@
 var adventureGirl;
 var adventureGirlObjects = [];
 var adventureGirlRunObjects = [];
+var adventureGirlMeleeObjects = [];
 var idleAnim = [];
 var runAnim = [];
+var meleeAnim = [];
 var x = 0;
 var y = 0;
-var i = 0;
-
-
-
+var run = 0;
+var idle = 0;
+var attack = 0;
+var speed = 10;
 
 function preload(){
 
-  // Challenge FAILED
-  // Idle assets
+  // Challenge Accepted
 
+  // Idle assets
   for (var i = 0; i < 9; i++)
   {
-    adventureGirl = new character(`assets/adventure girl/Idle (${i+1}).png`,0,0);
+    adventureGirl = new character(`assets/adventure girl/Idle (${i+1}).png`,x,y);
     adventureGirlObjects [i] = adventureGirl;
   }
 
   // Run assets
   for (var i = 0; i < 7; i++)
   {
-    adventureGirl = new character(`assets/adventure girl/Run (${i+1}).png`,0,0);
+    adventureGirl = new character(`assets/adventure girl/Run (${i+1}).png`,x,y);
     adventureGirlRunObjects [i] = adventureGirl;
   }
 
+  //Melee assets
+  for (var i = 0; i < 6; i++)
+  {
+    adventureGirl = new character(`assets/adventure girl/Melee (${i+1}).png`,x,y);
+    adventureGirlMeleeObjects [i] = adventureGirl;
+  }
 
-  //adventureGirl = new character('assets/Idle (1).png',0,0);
-  //adventureGirlObjects[0] = adventureGirl;
   // Idle Array Load
-  for (var i = 0; i < 9; i++)
+  for (var i = 0; i < adventureGirlObjects.length; i++)
   {
     idleAnim[i] = adventureGirlObjects[i].getImage();
   }
 
   // Run Array Load
-  for (var i = 0; i < 7; i++)
+  for (var i = 0; i < adventureGirlRunObjects.length; i++)
   {
     runAnim[i] = adventureGirlRunObjects[i].getImage();
+  }
+
+  //Melee Array Load
+  for (var i = 0; i < adventureGirlMeleeObjects.length; i++)
+  {
+    meleeAnim[i] = adventureGirlMeleeObjects[i].getImage();
   }
 
 }
 
 function setup(){
-  createCanvas(800,600);
-  
+  createCanvas(1200,800);
+
+  //Increment Idle
+  function incrementIdle()
+  {
+    idle++;
+
+    if (idle >= idleAnim.length)
+  {
+    idle = 0;
+  }
+  }
+  setInterval(incrementIdle,50);
+
+  //Increment Run
+  function incrementRun()
+  {
+    run++;
+
+    if (run >= runAnim.length)
+  {
+    run = 0;
+  }
+  }
+  setInterval(incrementRun,50);
+
+  //Increment Melee
+  function incrementAttack()
+  {
+    attack++;
+
+    if (attack >= meleeAnim.length)
+  {
+    attack = 0;
+  }
+  }
+  setInterval(incrementAttack,50);
 }
 
 function draw(){
 
   background(0);
-  image(idleAnim[i],adventureGirlObjects[i].getX,adventureGirlObjects[i].getY);
-  i++;
-
-  if (i >= idleAnim.length)
+  if (keyIsPressed)
   {
-    i = 0;
+    if (key === "w")
+    {
+      image(runAnim[run],x,y);
+      y-=speed;
+    }
+    if (key === "s")
+    {
+      image(runAnim[run],x,y);
+      y+=speed;
+    }
+    if (key === "a")
+    {
+      image(runAnim[run],x,y);
+      x-=speed;
+    }
+    if (key === "d")
+    {
+      image(runAnim[run],x,y);
+      x+=speed;
+    }
+    if ((key === "x") || (keyCode === 32))
+    {
+      image(meleeAnim[attack],x,y);
+    }
   }
+  else
+  {
+    image(idleAnim[idle],x,y);
+  }
+  
+
+  
+  
+ 
+  
 
 }
