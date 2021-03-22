24a66f4fa519515f36016a80692e459280d65d74
ThomasKeith <46757607+ThomasKeith@users.noreply.github.com>
2021-03-22 16:24:24 -0600

    Trying to figure it out

--- Week 10/js/sketch.js
+++ Week 10/js/sketch.js
@@ -5,11 +5,23 @@ let vid;
 
 
 
-
-
 function setup() {
 
+    //TEST CODE
+    noCanvas();
+    resultVideos = loadStrings('assets/videos.txt');
+    vid = createVideo([resultVideos[0]], vidLoad);
+    vid.size(100, 100);
+
+    function vidLoad() {
+        vid.loop();
+        vid.volume(0);
+    }
+
+
+
     // SETUP VIDEOS
+    /*
 
     resultVideos = loadStrings('assets/videos.txt');
 
@@ -17,23 +29,25 @@ function setup() {
         videoObjects.push(new videoClass(resultVideos[i], 100, 100, 100, 100));
     }
 
-    // CREATE CANVAS & BG
-    createCanvas(windowWidth, windowHeight);
+    // CANVAS
+    createCanvas(screen.width,screen.height);
     background(70);
 
 
     // PLAY VIDEO
     for (let i = 0; i < videoObjects.length; i++) {
-        let tempVideo = createVideo(videoObjects[i].theVideoPath);
+        let tempVideo = createVideo(videoObjects[i].theVideoPath, );
         tempVideo.size(videoObjects[i].theW, videoObjects[i].theH);
         tempVideo.position(videoObjects[i].theX, videoObjects[i].theY);
         videoObjects[i].setTheVideoObject(tempVideo);
-    }
 
+
+    }
     for (let i = 0; i < videoObjects.length; i++) {
         let temp = videoObjects[i].theVideoObject;
         temp.loop();
         temp.volume(0);
-    }
+    }*/
+
 
 }
